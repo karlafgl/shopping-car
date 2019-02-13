e164ebfb3343d4665f9d6e9cac2ba8fb9cb8be39
Karlafgl <karlafgl@gmail.com>
2019-02-13 10:24:47 -0700

    Adding shopping car proyect

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// Eliminar de Local Storage
+localStorage.clear();
+
+// const encabezado = document.querySelector('#encabezado');
+// const enlaces = document.querySelectorAll('.enlace')
+
+// const boton = document.querySelector('#vaciar-carrito')
+
+// const busqueda = document.querySelector('#buscador')
+
+// console.log(busqueda)
+
+
+// busqueda.addEventListener('keypress', function(e){
+
+//     // console.log(`Event: ${e.type}`)
+// })
+
+// document.body.addEventListener("click", function(e){
+//     if(e.target.classList.contains('borrar-curso')) {
+//         console.log(e.target.parentElement.parentElement.remove())
+//     } else {
+//         console.log('no')
+//     }
+//     if(e.target.classList.contains('agregar-carrito')) {
+//         console.log("Agregado")
+//     }
+// })
+
+//Agregar al local Storage
+localStorage.setItem('nombre', 'Juan')
+
+//Seccion storage
+secc
