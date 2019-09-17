99499997ca5ed8c96a9411fda3a658d997aaa748
karla <karla@Threde-LLC.local>
2019-09-16 18:23:49 -0600

    adding gulp configuration

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+function css(){
+  return gulp
+    .src('scss/app.scss')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(sass({
+      outputStyle: 'expanded', //nested, compact, compressed
+    }))
+    .pipe(gulp.dest('css'));
+}
+
+//Registrar funciones como tareas 
+gulp.task('css', css);
