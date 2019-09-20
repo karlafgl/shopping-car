84907c4e483f7920d5f7d8a994bb2c83b2179612
karlafgl <karlafgl@gmail.com>
2019-09-20 11:32:52 -0600

    fixing error

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,25 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-function css(){
-  return gulp
-    .src('scss/app.scss')
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(sass({
-      outputStyle: 'expanded', //nested, compact, compressed
-    }))
-    .pipe(gulp.dest('css'));
+function css() {
+    return gulp
+        .src('scss/app.scss')
+        .pipe(autoprefixer({
+            overrideBrowserslist : ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sass({
+            outputStyle: 'expanded', // nested, compact, compressed
+        }))
+        .pipe( gulp.dest('css') );
 }
 
-//function que vigilara por cambios en los archivos
-function watchFiles(){
-  gulp.watch('scss/*scss', css);
-  gulp.watch('index.html');
+function watchFiles() {
+    gulp.watch('scss/*.scss', css); 
+    gulp.watch('index.html');
 }
 
-//Registrar funciones como tareas 
-gulp.task('css', css);
-gulp.task('watch', gulp.parallel(watchFiles));
+
+// Registrar funciones como tareas
+gulp.task( 'css', css );
+gulp.task('watch', gulp.parallel( watchFiles) );
